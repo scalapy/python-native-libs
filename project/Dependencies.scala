05a22a1270ebb74296df9fc8270aa6165f8f0361
Scala Steward <me@scala-steward.org>
2022-01-12 10:36:39 +0100

    Update scala-collection-compat to 2.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaCollectionCompat =
-    "org.scala-lang.modules" %% "scala-collection-compat" % "2.4.4"
+    "org.scala-lang.modules" %% "scala-collection-compat" % "2.6.0"
   lazy val scalaTest       = "org.scalatest"        %% "scalatest"        % "3.2.9"
   lazy val jimfs           = "com.google.jimfs"      % "jimfs"            % "1.2"
   lazy val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.5.0"
