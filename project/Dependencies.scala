6f84c0584041a54f0a7088596912e19ce6b34d81
Scala Steward <me@scala-steward.org>
2022-04-25 10:05:04 +0200

    Update scalatest to 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val scalaCollectionCompat =
     "org.scala-lang.modules" %% "scala-collection-compat" % "2.7.0"
-  lazy val scalaTest       = "org.scalatest"        %% "scalatest"        % "3.2.11"
+  lazy val scalaTest       = "org.scalatest"        %% "scalatest"        % "3.2.12"
   lazy val jimfs           = "com.google.jimfs"      % "jimfs"            % "1.2"
   lazy val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.5.0"
 }
