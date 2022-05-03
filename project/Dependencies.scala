bee2892d8461e2e8a195436dbf4ab6c0ff093b0c
Kien Dang <mail@kien.ai>
2022-05-03 08:31:00 +0800

    Downgrade scala-collection-compat for native compatibility

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalapyVersion               = "0.5.2"
-  lazy val scalaCollectionCompatVersion = "2.7.0"
+  lazy val scalaCollectionCompatVersion = "2.5.0"
   lazy val scalaTestVersion             = "3.2.11"
 
   lazy val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.6.0"
