04c54444884bc3cf5596943b8c29bb80e855384b
Scala Steward <scala_steward@virtuslab.com>
2024-04-17 15:48:56 +0000

    Update scala-collection-compat to 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaCollectionCompat =
-    "org.scala-lang.modules" %% "scala-collection-compat" % "2.7.0"
+    "org.scala-lang.modules" %% "scala-collection-compat" % "2.12.0"
   lazy val scalapy         = "me.shadaj"            %% "scalapy-core"     % "0.5.2"
   lazy val scalaTest       = "org.scalatest"        %% "scalatest"        % "3.2.11"
   lazy val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.6.0"
