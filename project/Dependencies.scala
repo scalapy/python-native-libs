43bb410940e1528b7d7bbd0e1d5c2ff08ba2bc64
Scala Steward <scala_steward@virtuslab.com>
2024-06-24 17:20:18 +0000

    Update scalatest to 3.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
   lazy val scalaCollectionCompat =
     "org.scala-lang.modules" %% "scala-collection-compat" % "2.7.0"
   lazy val scalapy         = "me.shadaj"            %% "scalapy-core"     % "0.5.2"
-  lazy val scalaTest       = "org.scalatest"        %% "scalatest"        % "3.2.11"
+  lazy val scalaTest       = "org.scalatest"        %% "scalatest"        % "3.2.19"
   lazy val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.6.0"
 }
